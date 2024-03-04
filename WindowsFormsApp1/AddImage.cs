a741e1d80a3ccd2c05569dfe382b224ff631857c
Leonid29 <job@leonid29-developer.ru>
2024-03-04 21:46:15 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/AddImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guide_Diseases
+{
+    class AddImage
+    {
+        public AddImage()
+        {
+            // Сохранение изображения
+            string filename = @"C:\Users\User\Desktop\Проект\Фото\max_g480_c12_r2x3_pd20 (17).jpeg"; byte[] imageData;
+            using (FileStream fs = new FileStream(filename, FileMode.Open)) { imageData = new byte[fs.Length]; fs.Read(imageData, 0, imageData.Length); }
+
+            using (SqlConnection SQL_Connection = new SqlConnection(Main.ConnectString))
+            {
+                SQL_Connection.Open(); SqlCommand SQL_Command = SQL_Connection.CreateCommand();
+                string Request = $"UPDATE [Guide. Diseases].[dbo].[Images] SET [Image] = @ImageData WHERE [ID] = 'I018';"; // SQL-запрос
+                SQL_Command.Parameters.Add("@ImageData", SqlDbType.Image, 1000000); SQL_Command.Parameters["@ImageData"].Value = imageData;
+                SQL_Command.CommandText = Request; SQL_Command.ExecuteNonQuery(); SQL_Connection.Close();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/SummaryDiseases.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Guide_Diseases
+{
+    class SummaryDiseases
+    {
+        public string Disease { get; private set; }
+        public byte[] Image { get; private set; }
+        public List<Descriptions> Description { get; private set; }
+        public List<Agricultures> Agriculture { get; private set; }
+        public List<PreventionMethods> PreventionMethod { get; private set; }
+        public List<RecognitionMethods> RecognitionMethod { get; private set; }
+        public List<StruggleMethods> StruggleMethod { get; private set; }
+
+        public SummaryDiseases
+            (
+                string NewDisease, byte[] NewImage, List<Descriptions> NewDescription, List<Agricultures> NewAgriculture,
+                List<PreventionMethods> NewPreventionMethod, List<RecognitionMethods> NewRecognitionMethod, List<StruggleMethods> NewStruggleMethod
+            )
+        {
+            Disease = NewDisease; Image = NewImage; Description = NewDescription; Agriculture = NewAgriculture; 
+            PreventionMethod = NewPreventionMethod;  RecognitionMethod = NewRecognitionMethod; StruggleMethod = NewStruggleMethod;
+        }
+    }
+
+    class Descriptions
+    {
+        public string Text { get; private set; }
+        public Descriptions(string Description) {  Text = Description; }
+    }
+
+    class Agricultures
+    {
+        public string Text { get; private set; }
+        public Agricultures(string Agriculture) { Text = Agriculture; }
+    }
+
+    class PreventionMethods
+    {
+        public string Text { get; private set; }
+        public PreventionMethods(string PreventionMethod) { Text = PreventionMethod; }
+    }
+
+    class RecognitionMethods
+    {
+        public string Text { get; private set; }
+        public RecognitionMethods(string RecognitionMethod) { Text = RecognitionMethod; }
+    }
+
+    class StruggleMethods
+    {
+        public string Text { get; private set; }
+        public StruggleMethods(string StruggleMethod) { Text = StruggleMethod; }
+    }
+}
